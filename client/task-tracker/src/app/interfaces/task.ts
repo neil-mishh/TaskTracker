4bc009f302c7e9efe1bedf223c97ea2aa00e54aa
neil_mishh <thensm9neil@gmail.com>
2025-04-25 22:21:20 +1200

    create base components, service and interface for task

---
+++ client/task-tracker/src/app/interfaces/task.ts
@@ -0,0 +1,10 @@
+export interface Task {
+    Id: number;
+    Name: string;
+    Description: string;
+    CreatedAt: string;
+    DueDate: string;
+    IsCompleted: boolean;
+    Priority: string;
+    UserId: number;
+}
